134a5d8243a2a306b937be4f5be04a57e3c43948
Aui Santos <auisantos09@gmail.com>
2026-03-16 20:09:59 +0800

    feat(lambda): fix file path

--- main.tf
+++ main.tf
@@ -20,12 +20,12 @@ resource "aws_lambda_layer_version" "requests_layer" {
 }
 
 resource "aws_lambda_layer_version" "tenacity_py_layer" {
-  filename   = "${path.module}/lambda_layer_files/tenacity_py_layer.zip"
+  filename   = "${path.module}/lambda_layer_files/tenacity_package.zip"
   layer_name = "tenacity_py_layer"
   description = "A Lambda layer containing the tenacity library for Python"
   compatible_runtimes = ["python3.14"]
   
-  source_code_hash = filebase64sha256("${path.module}/lambda_layer_files/tenacity_py_layer.zip" )
+  source_code_hash = filebase64sha256("${path.module}/lambda_layer_files/tenacity_package.zip" )
 }
 
 ####################################################################
