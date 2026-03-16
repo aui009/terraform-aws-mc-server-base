be147bf59a92536a1e51f95384b3aa83bd08f3b8
Aui Santos <auisantos09@gmail.com>
2026-03-16 16:25:07 +0800

    feat(s3): update backup script

--- S3_files/mc-static-files/bedrock/scripts/backup_mc_files.sh
+++ S3_files/mc-static-files/bedrock/scripts/backup_mc_files.sh
@@ -3,21 +3,28 @@
 set -e 
 
 # Backup Minecraft Bedrock world files to S3
-backup_dir="opt/minecraft/backups/"
+backup_dir="/opt/minecraft/backups/"
 backup_file="bedrock_world_backup_$(date +%Y%m%d%H%M%S).zip"
 target_s3_path="s3://mc-server-ap-southeast-1-config-files-dev/bedrock/backup/"
-working_dir="/opt/minecraft/server/worlds/'Bedrock level'/"
+working_dir="/opt/minecraft/server/worlds/Bedrock level/"
 
 # shutdown the Bedrock server before backup
+echo "$(date +'%Y-%m-%d %H:%M:%S'): Stopping Bedrock server for backup"
 sudo systemctl stop bedrock.service
 
 # create backup files from the world files
-cd $working_dir
-sudo zip -r $backup_dir/$backup_file ./*
+echo "$(date +'%Y-%m-%d %H:%M:%S'): Creating backup of Bedrock world files"
+cd "$working_dir"
+sudo zip -r $backup_dir$backup_file ./
 
+echo "$(date +'%Y-%m-%d %H:%M:%S'): Backup created at $backup_dir$backup_file"
 # Upload the backup file to S3
-sudo aws s3 cp $backup_dir/$backup_file $target_s3_path
+echo "$(date +'%Y-%m-%d %H:%M:%S'): Uploading backup file to S3 at $target_s3_path$"
+sudo aws s3 cp $backup_dir$backup_file $target_s3_path
 
+echo "$(date +'%Y-%m-%d %H:%M:%S'): Backup file uploaded to S3 successfully"
+echo "$(date +'%Y-%m-%d %H:%M:%S'): Starting Bedrock server"
 #start the Bedrock server after backup
 sudo systemctl start bedrock.service
+echo "$(date +'%Y-%m-%d %H:%M:%S'): Bedrock world backup process completed"
 
