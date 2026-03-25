44fdc27ebcad545eeb232395e08d03f3dad02a42
Aui Santos <auisantos09@gmail.com>
2026-03-25 18:13:08 +0800

    feat(s3): add new files and updated scripts

--- S3_files/mc-static-files/java/scripts/monitor_activities_java_mc.sh
+++ S3_files/mc-static-files/java/scripts/monitor_activities_java_mc.sh
@@ -13,6 +13,8 @@ KEYWORD_DEATH_LIST=("${KEYWORD_DEATH_LIST[@]//\"/}")
 
 PLAYER_MESSAGE_PATTERN="<[^>]+>"
 
+LAMBDA_FUNCTION="mc_sendSQSPlayerEvent_handler"
+
 echo "$(date +'%Y-%m-%d %H:%M:%S'): Start Monitoring $LOG_FILE"
 echo "${KEYWORD_DEATH_LIST[@]}"
 tail -fn0 "$LOG_FILE" | while read -r LINE
@@ -22,15 +24,23 @@ do
            player_name=$(echo "$LINE" | awk '{print $4}')
            echo "$(date +'%Y-%m-%d %H:%M:%S'): player $player_name has joined"
            screen -S minecraft -X stuff 'say §aWelcome '"$player_name"' to the Server!\n'
-
+           detail_type="Player Joined Minecraft Server"
+           server_message="Player $player_name has joined the server"
+           aws lambda invoke --function-name $LAMBDA_FUNCTION --cli-binary-format raw-in-base64-out --payload '{"detail_type": "'"$detail_type"'", "server_message": "'"$server_message"'"}' output.json
         elif echo "$LINE" | grep -q "$PATTERN_LEFT" && ! echo "$LINE" | grep -qE "$PLAYER_MESSAGE_PATTERN";then
            player_name=$(echo "$LINE" | awk '{print $4}')
            echo "$(date +'%Y-%m-%d %H:%M:%S'): player $player_name has left"
            screen -S minecraft -X stuff 'say §9Goodbye '"$player_name"', hope to see you soon!\n'
+           detail_type="Player Left Minecraft Server"
+           server_message="Player $player_name has left the server"
+           aws lambda invoke --function-name $LAMBDA_FUNCTION --cli-binary-format raw-in-base64-out --payload '{"detail_type": "'"$detail_type"'", "server_message": "'"$server_message"'"}' output.json
 
         elif grep -iqFf  <(printf "%s\n" "${KEYWORD_DEATH_LIST[@]}") <<< "$LINE" && ! echo "$LINE" | grep -qE "$PLAYER_MESSAGE_PATTERN";then
            echo "$(date +'%Y-%m-%d %H:%M:%S'): A Player has died"
            DEATH_MESSAGE=$(echo "$LINE" | awk -F': ' '{print $2}')
            echo "$(date +'%Y-%m-%d %H:%M:%S'): $DEATH_MESSAGE"
+           detail_type="Player Died in Minecraft Server"
+           server_message=$DEATH_MESSAGE
+           aws lambda invoke --function-name $LAMBDA_FUNCTION --cli-binary-format raw-in-base64-out --payload '{"detail_type": "'"$detail_type"'", "server_message": "'"$server_message"'"}' output.json
         fi
 done
