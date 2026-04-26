c8661e94907d6ff34629cd08a3627b99d552ed08
Aui Santos <auisantos09@gmail.com>
2026-04-26 12:07:21 +0800

    feat: create new ssm document for automation

--- main.tf
+++ main.tf
@@ -150,6 +150,114 @@ resource "aws_ssm_document" "ssm_run_backup_script_command" {
   }
   DOC
 }
+
+resource "aws_ssm_document" "ssm_create_snapshot_ec2_command" {
+  name            = "CreateSnapshotEc2Command"
+  document_type   = "Automation"
+  document_format = "JSON"
+
+  content = jsondecode({
+    "schemaVersion" : "0.3",
+    "description" : "Create EBS volume snapshot",
+    "assumeRole" : "{{ AutomationAssumeRole }}",
+    "parameters" : {
+      "VolumeId" : {
+        "type" : "String",
+        "description" : "(Required) The ID of the volume."
+      },
+      "Description" : {
+        "type" : "String",
+        "description" : "(Optional) A description for the snapshot",
+        "default" : ""
+      },
+      "AutomationAssumeRole" : {
+        "type" : "String",
+        "description" : "(Optional) The ARN of the role that allows Automation to perform the actions on your behalf. ",
+        "default" : ""
+      }
+    },
+    "mainSteps" : [
+      {
+        "name" : "createSnapshot",
+        "action" : "aws:executeAwsApi",
+        "nextStep" : "CreateTags",
+        "isEnd" : false,
+        "inputs" : {
+          "Service" : "ec2",
+          "Api" : "CreateSnapshot",
+          "VolumeId" : "{{ VolumeId }}",
+          "Description" : "{{ Description }}"
+        },
+        "outputs" : [
+          {
+            "Name" : "Payload",
+            "Selector" : "SnapshotId",
+            "Type" : "String"
+          }
+        ]
+      },
+      {
+        "name" : "CreateTags",
+        "action" : "aws:executeAwsApi",
+        "nextStep" : "verifySnapshot",
+        "isEnd" : false,
+        "inputs" : {
+          "Service" : "ec2",
+          "Api" : "CreateTags",
+          "Tags" : [
+            {
+              "Key" : "CreatedBy",
+              "Value" : "SSMAutomation"
+            },
+            {
+              "Key" : "Backup",
+              "Value" : "True"
+            },
+            {
+              "Key" : "Environment",
+              "Value" : "Dev"
+            },
+            {
+              "Key" : "SnapshotId",
+              "Value" : "{{ createSnapshot.Payload }}"
+            }
+          ],
+          "Resources" : [
+            "{{ createSnapshot.Payload }}"
+          ]
+        },
+        "outputs" : [
+          {
+            "Type" : "String",
+            "Name" : "Payload",
+            "Selector" : "SnapshotId"
+          }
+        ]
+      },
+      {
+        "name" : "verifySnapshot",
+        "action" : "aws:waitForAwsResourceProperty",
+        "isEnd" : true,
+        "inputs" : {
+          "Service" : "ec2",
+          "Api" : "DescribeSnapshots",
+          "SnapshotIds" : [
+            "{{createSnapshot.Payload}}"
+          ],
+          "PropertySelector" : "Snapshots[0].State",
+          "DesiredValues" : [
+            "completed"
+          ]
+        }
+      }
+    ],
+    "outputs" : [
+      "createSnapshot.Payload"
+    ]
+    }
+
+  )
+}
 ####################################################################
 #                         AWS SQS Queues                           
 ####################################################################
